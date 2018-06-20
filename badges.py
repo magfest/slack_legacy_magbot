5b135ac3de49f7a7175934733634b4515fa26ba4
Rob Ruana <robruana@magfest.org>
2018-06-20 12:02:26 -0400

    Small tweaks to badges plugin

--- badges.py
+++ badges.py
@@ -1,69 +1,104 @@
-from errbot import BotPlugin, botcmd
-import requests
 import json
+import urllib
 
-class Badges(BotPlugin): 
+import requests
+from errbot import BotPlugin, botcmd
+
+
+def _normalize_url(url):
+    url = (url or '').strip()
+    if not url:
+        return ''
+
+    scheme, _, location = url.rpartition('://')
+    domain, _, path = location.partition('/')
+    subdomain, _, basedomain = domain.partition('.')
+
+    scheme = scheme or 'https'
+
+    path = path.strip('/')
+    if not path:
+        path = 'uber/registration/stats'
+    elif not path.endswith('registration/stats'):
+        path += '/registration/stats'
+
+    if not basedomain:
+        domain = subdomain + '.uber.magfest.org'
+
+    return (subdomain, '{}://{}/{}'.format(scheme, domain, path))
+
+
+def _format_events(responses):
+    message = ['|Event|Price|Sold|Remaining|', '|:---|---:|---:|---:|']
+    for name in sorted(responses.keys(), key=lambda s: s.lower()):
+        response = responses[name]
+        message.append('|{name}|${price}|{sold}|{remaining}|'.format(
+            name=name,
+            price=response['badges_price'],
+            sold=response['badges_sold'],
+            remaining=response['remaining_badges']))
+    return '\n'.join(message)
+
+
+class Badges(BotPlugin):
     @botcmd
     def badges(self, mess, args):
-        """ Shows badge counts for all current Mag events """        
+        """Display badge counts for current MAGFest events."""
         if len(self) > 0:
-            response = '|Event|Price|Sold|Remaining|\n|:---|---:|---:|---:|\n'
-            for name in self:
-                badgeResponse = requests.get(self[name]).json()
-                #yield str (badgeResponse)
-                row = '|{name}|{price}|{sold}|{remaining}|\n'.format(
-                    name=name,
-                    price='$' + str(badgeResponse['badges_price']),
-                    sold=badgeResponse['badges_sold'],
-                    remaining=badgeResponse['remaining_badges'])
-                response += row
-            return response
-                
-        else:
-            return u'No events currently in list. ' \
-                    'Use {prefix}badges event add to define one.'.format(
-                        prefix=self._bot.prefix
-                    )
+            return _format_events({s: requests.get(self[s]).json() for s in self})
+        return 'No events currently in list. You can add an event with the following command:\n' \
+            '`{}badges event add [<name>] <url>`'.format(self._bot.prefix)
 
     @botcmd(split_args_with=None)
     def badges_event_add(self, mess, args):
-        """ Add a new event to the events checked when asking for badge counts """
-        if len(args) < 2:
-            return u'usage: {prefix}badges event add <name> <url>'.format(
-                prefix=self._bot.prefix
-            )
-        name = args[0]
-        url = args[1] 
+        """Add an event to the list of events checked for badge counts."""
+        if len(args) < 1:
+            return 'Usage: `{}badges event add [<name>] <url>`'.format(self._bot.prefix)
+        elif len(args) == 1:
+            raw_url = args[0]
+            name, url = _normalize_url(raw_url)
+        else:
+            name = args[0]
+            raw_url = args[1]
+            _, url = _normalize_url(raw_url)
+
+        try:
+            response = requests.get(url).json()
+        except Exception as ex:
+            message = ['Error contacting given url: {}'.format(raw_url)]
+            if raw_url != url:
+                message.append('Tried to contact: {}'.format(url))
+            message.append('\`\`\`')
+            message.append(str(ex))
+            message.append('\`\`\`')
+            return '\n'.join(message)
+
         self[name] = url
-        return u'Event added to list.'
-    
+        return 'Event "{}" added to list:\n\n{}'.format(name, _format_events({name: response}))
+
     @botcmd(split_args_with=None)
     def badges_event_remove(self, mess, args):
-        """ Remove an event from the events checked when asking for badge counts """
+        """Remove an event from the list of events checked for badge counts."""
         if len(args) < 1:
-            return u'usage: {prefix}badges event remove <name>'.format(
-                prefix=self._bot.prefix
-            )
+            return 'Usage: `{}badges event remove <name>`'.format(self._bot.prefix)
         name = args[0]
         try:
             del self[name]
-            return u'Event removed from list.'
+            return 'Event "{}" removed from list.'.format(name)
         except KeyError:
-            return u'That event is not in the list. ' \
-                   'Use {prefix}badges events list to see all events.'.format(
-                       prefix=self._bot.prefix
-                   )
+            return 'The event "{}" is not in the list. ' \
+                'You can view the event list with the following command:\n' \
+                '`{}badges event list`'.format(name, self._bot.prefix)
+
     @botcmd
     def badges_event_list(self, mess, args):
         """List all events to check badges along with the URL for each."""
         if len(self) > 0:
-            return u'|Name|URL|\n|:---|:---|\n' + u'\n'.join(
+            return '|Name|URL|\n|:---|:---|\n' + '\n'.join(
                 ['|{name}|{url}|'.format(
                     name=name,
                     url=self[name]
                 ) for name in self])
-        else:
-            return u'No events currently in list. ' \
-                   'Use {prefix}badges event add to define one.'.format(
-                       prefix=self._bot.prefix
-                   )
+
+        return 'No events currently in list. You can add an event with the following command:\n' \
+            '`{}badges event add [<name>] <url>`'.format(self._bot.prefix)
