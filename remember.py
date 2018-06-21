13a24fa05a9511ade5976cfc6229b153b683e26b
Rob Ruana <robruana@magfest.org>
2018-06-20 23:25:22 -0400

    Removes janky sync code, need to switch to redis storage

--- remember.py
+++ remember.py
@@ -8,10 +8,6 @@ _REMEMBER_SPLIT_RE = re.compile(r'\s+is[\s\n]+', flags=re.IGNORECASE)
 
 class Remember(BotPlugin):
 
-    def _sync(self):
-        if getattr(getattr(self, '_store'), 'shelf'):
-            self._store.shelf.sync()
-
     def _get_memory(self, key, escape=True):
         value = self.get('memories', {})[key.lower()]
         if escape:
@@ -25,7 +21,6 @@ class Remember(BotPlugin):
         else:
             memories[key.lower()] = value
         self['memories'] = memories
-        self._sync()
 
     @re_botcmd(
         re_cmd_name_help='remember',
