1865a17a0b102833c4f9bbc05e1a7863ceb5f570
Daniel Evans <migetman9@gmail.com>
2018-07-03 23:09:21 -0700

    Fix bugs after local testing

--- archive.py
+++ archive.py
@@ -6,8 +6,8 @@ class Archive(BotPlugin):
 
     def _get_timestamp_for_channel(self, channel_id):
         try:
-            messages = self._bot.sc.api_call('channels.history', channel=channel_id, count=1)
-            return float(messages[0]['ts'])
+            message = self._bot.sc.api_call('channels.history', channel=channel_id, count=1)['messages'][0]
+            return float(message['ts'])
         except Exception:
             return None
 
@@ -16,8 +16,9 @@ class Archive(BotPlugin):
         """List all channels whose most recent message is at least the given number of days old. Defaults to 90 days."""
         channels = self._bot.channels()
         message = []
+        args = args
         try:
-            days = int(args) if int(args) > 1 else 90
+            days = int(args) if args else 90
         except ValueError:
             return 'Usage: `!archive list [DAYS]` where DAYS is a number like 30 or 365. Defaults to 90'
         archive_past_this_date = datetime.now() - timedelta(days=days)
@@ -26,9 +27,9 @@ class Archive(BotPlugin):
             if timestamp:
                 channel_date = datetime.utcfromtimestamp(timestamp)
                 if channel_date < archive_past_this_date:
-                    age = (archive_past_this_date - channel_date).days
-                    message.append('<#{}|{}> has not had activity in {} days.'.format(channel['id'],
-                                                                                      channel['name'], age))
+                    age = (datetime.now() - channel_date).days
+                    message.append('<#{}|{}> has not had activity in {} days'.format(channel['id'], channel['name'],
+                                                                                     age))
         if len(message) == 0:
             message.append('No channels that haven\'t been used in the last {} days.'.format(days))
 
