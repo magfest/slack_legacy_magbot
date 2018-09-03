4ff4451bf33696fa1ee7aae3ff61bad752ccf7d3
Rob Ruana <robruana@magfest.org>
2018-09-03 11:13:29 -0400

    Trying to get imports for new file layout working

--- magbot/infrastructure.py
+++ magbot/infrastructure.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from collections import OrderedDict
 from functools import wraps
