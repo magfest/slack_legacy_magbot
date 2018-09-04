69d9f063d7e36d35e56801cfbb0e524a31b9d9ba
Rob Ruana <robruana@magfest.org>
2018-09-04 05:13:54 -0400

    Debugging a python path issue

--- magbot/infrastructure.py
+++ magbot/infrastructure.py
@@ -1,7 +1,4 @@
-import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))  # noqa: E402
-
 from collections import OrderedDict
 from functools import wraps
 
--- magbot/links.py
+++ magbot/links.py
@@ -1,7 +1,3 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))  # noqa: E402
-
 import random
 import re
 from functools import reduce
